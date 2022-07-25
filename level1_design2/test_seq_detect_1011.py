560bc41936b5fed8dd419a79d3f7da278eeff177
arka-23 <70422874+arka-23@users.noreply.github.com>
2022-07-25 10:32:02 +0530

    Update test_seq_detect_1011.py

--- level1_design2/test_seq_detect_1011.py
+++ level1_design2/test_seq_detect_1011.py
@@ -2,6 +2,7 @@
 
 # SPDX-License-Identifier: CC0-1.0
 
+
 import os
 import random
 from pathlib import Path
@@ -20,7 +21,35 @@ async def test_seq_bug1(dut):
     # reset
     dut.reset.value = 1
     await FallingEdge(dut.clk)  
-    dut.reset.value = 0
-    await FallingEdge(dut.clk)
+    dut.reset.value = 0    
+    #await FallingEdge(dut.clk)
 
     cocotb.log.info('#### CTB: Develop your test here! ######')
+    x = [1,0,1,1]
+    for i in range(2):
+        bit = random.randint(0,1)
+        dut.inp_bit.value = bit  
+        A = 0
+        x.insert(i, bit)
+        await FallingEdge(dut.clk)   
+        dut._log.info(f'bit={bit:01}  model={A:01} DUT={int(dut.seq_seen.value):01}')          
+
+    #await FallingEdge(dut.clk)
+    dut.inp_bit.value = 1
+    await FallingEdge(dut.clk)
+    dut.inp_bit.value = 0
+    await FallingEdge(dut.clk)
+    dut.inp_bit.value = 1
+    await FallingEdge(dut.clk)
+    dut.inp_bit.value = 1     
+    
+        
+    #await FallingEdge(dut.clk)
+    A = 1
+        #await Timer(2, units='ns')        
+    await FallingEdge(dut.clk)    
+    dut._log.info(f'bit={1:01}  model={A:01} DUT={int(dut.seq_seen.value):01}')
+
+    assert dut.seq_seen.value == A, "Randomised test failed with: sequence - {x} ".format(x = x)
+        
+        
